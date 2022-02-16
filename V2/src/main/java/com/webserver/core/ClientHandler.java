f8065b847f56519f797648d6ff48a83e0b1f5df8
Dinuos <2893416367@qq.com>
2022-02-16 15:56:21 +0800

    my server

--- V2/src/main/java/com/webserver/core/ClientHandler.java
+++ V2/src/main/java/com/webserver/core/ClientHandler.java
@@ -1,7 +1,16 @@
 package com.webserver.core;
 
+import java.net.Socket;
+
 /**
  * 该线程任务负责与指定的客户端完成HTTP交互
  */
-public class ClientHandler {
+public class ClientHandler implements Runnable {
+    private Socket socket;
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+    public void run() {
+
+    }
 }
